7e8074f31176aa58fc7f59826fd7bd8cd5bc6dc0
Boris Buliga <boris@d12frosted.io>
2018-01-23 21:46:31 +0200

    add dummy posts compiler

--- app/Main.hs
+++ app/Main.hs
@@ -30,12 +30,35 @@ main = hakyll $ do
   match "static/index.html" $ do
     route $ gsubRoute "static/" (const "")
     compile $ do
-      context <- loadContext
+      ctx <- loadIndexCtx
       getResourceBody
-        >>= loadAndApplyTemplate "templates/default.html" context
+        >>= loadAndApplyTemplate "templates/default.html" ctx
+        >>= relativizeUrls
+
+  match "posts/*.markdown" $ do
+    route $ setExtension "html"
+    compile $ do
+      ctx <- loadPostCtx
+      pandocCompiler
+        >>= loadAndApplyTemplate "templates/post.html"    ctx
+        >>= loadAndApplyTemplate "templates/default.html" ctx
         >>= relativizeUrls
 
   match "templates/*" $ compile templateBodyCompiler
 
-loadContext :: Compiler (Context String)
-loadContext = appContext <$> itemBody <$> load "configs/config.json"
+--------------------------------------------------------------------------------
+loadCtx :: Compiler (Context String)
+loadCtx = appContext <$> itemBody <$> load "configs/config.json"
+
+loadIndexCtx :: Compiler (Context String)
+loadIndexCtx = constField "group" "index" <+> loadCtx
+
+loadPostCtx :: Compiler (Context String)
+loadPostCtx = constField "group" "post"
+  <+> dateField "date" "%B %e, %Y"
+  <+> loadCtx
+
+--------------------------------------------------------------------------------
+(<+>) :: (Monoid a, Applicative m) => a -> m a -> m a
+a <+> ma = mappend a <$> ma
+infixr 6 <+>
