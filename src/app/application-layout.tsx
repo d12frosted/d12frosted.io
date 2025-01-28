a6f118de3b3c2bf4504725fd29688c8839e7c5ea
Boris Buliga <boris@d12frosted.io>
2025-01-28 09:22:54 +0200

    remove unused code

--- src/app/application-layout.tsx
+++ src/app/application-layout.tsx
@@ -1,24 +1,16 @@
+import { WineIcon } from '@/components/icons/WineIcon'
 import { Navbar, NavbarDivider, NavbarItem, NavbarLabel, NavbarSection, NavbarSpacer } from '@/components/navbar'
 import { Sidebar, SidebarBody, SidebarHeader, SidebarItem, SidebarSection } from '@/components/sidebar'
 import { StackedLayout } from '@/components/stacked-layout'
+import { siteConfig } from '@/config/config'
+import { SiGithub } from '@icons-pack/react-simple-icons'
 import Image from 'next/image'
-import {SiGithub} from "@icons-pack/react-simple-icons";
-import {WineIcon} from "@/components/icons/WineIcon";
-import {siteConfig} from "@/app/config/config";
-import React from "react";
+import React from 'react'
 
 const navItems = [
   { label: 'Home', url: '/' },
   { label: 'Posts', url: '/posts' },
 ]
-
-function OpenMenuIcon() {
-  return (
-    <svg data-slot="icon" viewBox="0 0 20 20" aria-hidden="true">
-      <path d="M2 6.75C2 6.33579 2.33579 6 2.75 6H17.25C17.6642 6 18 6.33579 18 6.75C18 7.16421 17.6642 7.5 17.25 7.5H2.75C2.33579 7.5 2 7.16421 2 6.75ZM2 13.25C2 12.8358 2.33579 12.5 2.75 12.5H17.25C17.6642 12.5 18 12.8358 18 13.25C18 13.6642 17.6642 14 17.25 14H2.75C2.33579 14 2 13.6642 2 13.25Z" />
-    </svg>
-  )
-}
 
 export function ApplicationLayout({
   // events,
