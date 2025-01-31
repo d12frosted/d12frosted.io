76606ea9a1ff7a7d458c5e4329de4ebf605b55e8
Boris Buliga <boris@d12frosted.io>
2025-01-31 23:51:34 +0200

    remove unnecessary py-8

--- src/components/blog/posts.tsx
+++ src/components/blog/posts.tsx
@@ -37,7 +37,7 @@ export function LatestPosts({ allPosts, ...props }: React.ComponentProps<'div'> 
     <div {...props}>
       <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
         {posts.map((post) => (
-          <FeaturedPostCard key={post.id} post={post} className="py-8" />
+          <FeaturedPostCard key={post.id} post={post} />
         ))}
       </div>
     </div>
