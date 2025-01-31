1971ab40036fe15a4cc865e0c7753f703ef441dd
Boris Buliga <boris@d12frosted.io>
2025-01-31 23:56:30 +0200

    improve post page

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -66,26 +66,25 @@ export default async function Post({ params }: Props) {
   let context = { post, allPosts }
 
   return (
-    <article className="py-8">
+    <article className="">
       <header className="flex flex-col">
-        <div className="flex items-center gap-6">
+        <div className="flex items-center gap-x-4 text-xs">
           <div className="flex flex-col">
-            <h1 className="mt-2 text-4xl font-bold text-slate-900">{post.title}</h1>
-            <div className="order-first flex items-center gap-4 font-mono text-sm leading-7 text-slate-500">
-              <FormattedDate date={date}/>
-              <p>·</p>
-              <p>{post.language}</p>
-              <p>·</p>
-              <p>{post.authors.join(', ')}</p>
+            <div className="flex items-center gap-x-4 text-xs">
+              <FormattedDate date={post.published} className="text-gray-500" />
+              <div className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">
+                {post.tags.join(' · ')}
+              </div>
             </div>
+            <h1 className="mt-2 text-4xl font-bold text-slate-900">{post.title}</h1>
           </div>
         </div>
-        <p className="mt-3 text-lg leading-8 font-medium text-slate-700">{post.description}</p>
+        <p className="mt-5 line-clamp-24 text-sm/6 text-gray-600">{post.description}</p>
       </header>
-      <hr className="my-8 border-gray-200"/>
+      <hr className="my-8 border-gray-200" />
       <div
         id="post-content"
-        className="prose max-w-none mt-14 prose-stone prose-h1:mt-6 prose-h1:mb-2 prose-h2:mt-4 prose-h2:mb-2 prose-p:mt-2 [&>ul]:mt-6 [&>ul]:list-['\2013\20'] [&>ul]:pl-5"
+        className="prose mt-14 max-w-none prose-stone prose-h1:mt-6 prose-h1:mb-2 prose-h2:mt-4 prose-h2:mb-2 prose-p:mt-2 [&>ul]:mt-6 [&>ul]:list-['\\2013\\20'] [&>ul]:pl-5"
       >
         <CustomMarkdown context={context}>{post.content}</CustomMarkdown>
       </div>
